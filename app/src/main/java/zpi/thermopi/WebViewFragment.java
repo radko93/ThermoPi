427b2792a6f57731dcc93c7b4ba5be13345bd0e4
Radoslaw Czemerys <radko93@gmail.com>
2015-06-05 02:18:18 +0200

    swipe to refresh added

--- app/src/main/java/zpi/thermopi/WebViewFragment.java
+++ app/src/main/java/zpi/thermopi/WebViewFragment.java
@@ -2,7 +2,9 @@ package zpi.thermopi;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +12,12 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 
-public class WebViewFragment extends Fragment {
+public class WebViewFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
 
     private int page;
     private WebView webView;
+    private SwipeRefreshLayout swipeLayout;
 
     public static WebViewFragment newInstance(int page) {
         WebViewFragment fragmentFirst = new WebViewFragment();
@@ -29,6 +32,20 @@ public class WebViewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         page = getArguments().getInt("someInt", 0);
+
+
+
+    }
+
+
+    @Override public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+
+                swipeLayout.setRefreshing(false);
+            }
+        }, 900);
+        webView.loadUrl("javascript:refresh()");
     }
 
 
@@ -36,21 +53,28 @@ public class WebViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_web_view, container, false);
-
         webView = (WebView) view.findViewById(R.id.web);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
 
 
-       switch (page)
-       {
-           case 0:
-               webView.loadUrl("http://thermopi.azurewebsites.net/PieChartMobile.html");
-               break;
-           case 1:
-               webView.loadUrl("http://thermopi.azurewebsites.net/LineChartMobile.html");
+        switch (page)
+        {
+            case 0:
+                webView.loadUrl("http://thermopi.azurewebsites.net/PieChartMobile.html");
+                break;
+            case 1:
+                webView.loadUrl("http://thermopi.azurewebsites.net/LineChartMobile.html");
+                break;
 
-       }
+        }
+
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
 
         return view;
